bb791a4b80fa15e1379dc12542cff9b25c8ecb6e
YukiKuroshima <yukikuro1993@gmail.com>
2017-08-03 15:00:26 -0700

    home

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
+/**
+ * impot libraries
+ */
 
+import express from 'express';
+
+const app = express();
+
+/**
+ * Configuration
+ */
+
+const port = process.env.PORT || 8080;
+
+app.listen(port);
+console.log(`Server running ${port}`);
