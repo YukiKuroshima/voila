74f07469bc3293b551727f2b378dae8e67786058
YukiKuroshima <yukikuro1993@gmail.com>
2017-08-03 16:34:40 -0700

    Set up done

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@
  * impot libraries
  */
 
-import express from 'express';
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -11,6 +12,19 @@ const app = express();
  */
 
 const port = process.env.PORT || 8080;
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// basic route (http://localhost:8080)
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+/**
+ * Start server
+ */
 
 app.listen(port);
 console.log(`Server running ${port}`);
